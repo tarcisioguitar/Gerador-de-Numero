abffcaa1ee95a7499227169b3d7b0b4f815e3119
tarcisioguitar <ciso_12@hotmail.com>
2020-08-04 21:16:05 -0300

    verificador de números repetidos

--- gerador de numero/Form1.cs
+++ gerador de numero/Form1.cs
@@ -60,16 +60,16 @@ namespace gerador_de_numero
             switch (qtdJogos)
             {
                 case 1:
-                    jogo1.Text = "Jogo 1: " + CriarJogo(qtdNumeros);
+                    jogo1.Text = "Jogo 1:   " + CriarJogo(qtdNumeros);
                     break;
                 case 2:
-                    jogo1.Text = "Jogo 1: " + CriarJogo(qtdNumeros);
-                    jogo2.Text = "Jogo 2: " + CriarJogo(qtdNumeros);
+                    jogo1.Text = "Jogo 1:   " + CriarJogo(qtdNumeros);
+                    jogo2.Text = "Jogo 2:   " + CriarJogo(qtdNumeros);
                     break;
                 case 3:
-                    jogo1.Text = "Jogo 1: " + CriarJogo(qtdNumeros);
-                    jogo2.Text = "Jogo 2: " + CriarJogo(qtdNumeros);
-                    jogo3.Text = "Jogo 3: " + CriarJogo(qtdNumeros);
+                    jogo1.Text = "Jogo 1:   " + CriarJogo(qtdNumeros);
+                    jogo2.Text = "Jogo 2:   " + CriarJogo(qtdNumeros);
+                    jogo3.Text = "Jogo 3:   " + CriarJogo(qtdNumeros);
                     break;
                 default:
                     MessageBox.Show("Houve algum erro");
@@ -77,7 +77,7 @@ namespace gerador_de_numero
             }
         }
 
-
+     
         public string CriarJogo(int qtdNumero)
         {
 
@@ -85,14 +85,20 @@ namespace gerador_de_numero
             string resultado = "";
             for (int i = 1; i <= qtdNumero; i++)
             {
-
                 numero = aleatorio.Next(1, 99).ToString("D2");
-                if(resultado.Contains(numero))
+                if (resultado.Contains(numero))
                 {
+                    numero = "";
                     numero = aleatorio.Next(1, 99).ToString("D2");
-                    resultado += numero + "    ";
-                                     
+                    if (resultado.Contains(numero))
+                    {
+                        MessageBox.Show($"Número repetido {numero}");
+                        numero = "";
+                        numero = aleatorio.Next(1, 99).ToString("D2");
+
+                    }
                 }
+
                 resultado += numero + "    ";
             }
 
